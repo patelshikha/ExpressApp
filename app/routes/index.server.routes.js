92d0280db53821c2d39ad2a54a0f0a10c3e8bdaf
Shikha Patel <spate830@my.centennialcollege.ca>
2020-04-13 22:48:05 -0400

    updated route file

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,23 +1,15 @@
-﻿// Load the 'index' controller
-const index = require('../controllers/index.server.controllers');
+// Load the 'index' controller
+const index = require('../controllers/index.server.controller');
 
-
-// Define the routes module' method
 module.exports = function (app) {
 
-    // Mount the 'index' controller's 'render' method
-    app.get('/', index.render);
-    //
-    app.get('/and_gate', index.andGate);
-    //
-    app.get('/results', index.linearClassifier);
-    //
-    app.get('/display', function (req, res) {
-
-        res.render('index1');
+    app.get('/', function (req, res) {
+        res.render('index', {
+            info: "see the results in console window"
+        })
     });
 
-    //
-    app.get('/function_classifier', index.functionClassifier);
-    
+    app.get('/run', index.trainAndPredict);
+    app.post('/manualData', index.manualData);
+
 };
